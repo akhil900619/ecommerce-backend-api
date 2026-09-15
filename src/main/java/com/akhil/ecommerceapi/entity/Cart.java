1de0d77b0b811d7fb9116a6cae653ccb59bf6abf
Akhil Sharma <akhil900619@gmail.com>
2026-09-15 12:36:42 +0530

    Add Cart and CartItem entities with OneToOne and OneToMany relationships

---
+++ src/main/java/com/akhil/ecommerceapi/entity/Cart.java
@@ -0,0 +1,28 @@
+package com.akhil.ecommerceapi.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "carts")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false, unique = true)
+    private User user;
+
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CartItem> items = new ArrayList<>();
+
+}
---
+++ src/main/java/com/akhil/ecommerceapi/entity/CartItem.java
@@ -0,0 +1,32 @@
+package com.akhil.ecommerceapi.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.*;
+
+@Entity
+@Table(name = "cart_items")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "cart_id", nullable = false)
+    private Cart cart;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @NotNull(message = "Quantity is required")
+    @Positive(message = "Quantity must be greater than zero")
+    @Column(nullable = false)
+    private Integer quantity;
+
+}
